53986b14399222aa227f99fb0a90673f5ab2c70f
dev-harsh1998 <harshit.india@linaro.org>
2018-02-17 07:03:32 +0100

    [FIXME]wt88047: disable media treble
    * Its breaking in app players due to OMX buffer underruns

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,3 +1,9 @@
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
+    $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
+
+# Temp disable media treble until i fix OMX decoder for inapp players
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false
+
